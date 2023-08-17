6e1a4b5ba66593da0ec48e07bbe39e6413324a89
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-16 22:51:54 -0400

    Angular Material - Autocomplete

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
 import { HeroImagePipe } from './pipes/hero-image.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -24,8 +25,10 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     HeroesRoutingModule,
-    MaterialModule
+    MaterialModule,
+
   ]
 })
 export class HeroesModule { }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -23,4 +23,8 @@ export class HeroesService {
       catchError(error => of(undefined))
     );
   }
+
+  getSuggestions(query:string): Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
 }
